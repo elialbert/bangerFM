16045d2c0357f749718c6d11dfc0bfc98afe588d
elialbert <elialbert@gmail.com>
2017-04-18 09:43:57 -0500

    clean up circle yml

--- build/deploy.sh
+++
@@ -1,3 +0,0 @@
-npm run build
-aws s3 sync dist/ s3://aws-website-bangerfm-3p7g1/ --delete
-aws cloudfront create-invalidation --cli-input-json "{\"DistributionId\":\"E17TETHXN4E0OU\",\"InvalidationBatch\":{\"Paths\":{\"Quantity\":1,\"Items\":[\"/*\"]},\"CallerReference\":\"$(date +%s)\"}}"

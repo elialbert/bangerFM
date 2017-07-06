84ff50197cbaa56e595a2760a54084c3df93be8d
elialbert <elialbert@gmail.com>
2017-07-06 09:53:49 -0500

    switching out to a landing

---
+++ src/components/landing/Landing.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="landing" ref='landing'>
+    testing landing
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landing',
+  components: {
+  },
+  data: function () {
+    return {
+    }
+  },
+  mounted: function () {
+  },
+  watch: {
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
